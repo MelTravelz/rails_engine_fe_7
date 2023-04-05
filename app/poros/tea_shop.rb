5593a20d14b95a6d7d2bad1d6d98e017f41232ab
Melony Franchini <meltravelz@gmail.com>
2023-04-05 16:33:33 -0600

    Update poro to include id

--- app/poros/tea_shop.rb
+++ app/poros/tea_shop.rb
@@ -1,7 +1,8 @@
 class TeaShop
-  attr_reader :name
+  attr_reader :name, :id
 
   def initialize(info_hash)
     @name = info_hash[:attributes][:name]
+    @id = info_hash[:id]
   end
 end
--- spec/poros/tea_shop_spec.rb
+++ spec/poros/tea_shop_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe TeaShop do
   let(:attributes) do
     tea_shop_list = {
+      id: 55,
       attributes: { name: "Celestial Seasonings" }
     }
   end
@@ -11,6 +12,7 @@ RSpec.describe TeaShop do
 
   it "exists & has attributes" do 
     expect(tea_shop).to be_a(TeaShop)
+    expect(tea_shop.id).to eq(55)
     expect(tea_shop.name).to eq("Celestial Seasonings")
   end
 end
