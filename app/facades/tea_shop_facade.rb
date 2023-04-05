8852232baedd16078965fb18e2da36a0ae93a44a
Melony Franchini <meltravelz@gmail.com>
2023-04-05 15:51:11 -0600

    Create #get_all_merchants endpoint - needs poro

--- app/facades/tea_shop_facade.rb
+++ app/facades/tea_shop_facade.rb
@@ -4,6 +4,9 @@ class TeaShopFacade
   end
 
   def get_all_merchants
-
+    search_results = tea_shop_service.get_all_merchants
+    search_results[:data].map do |merchant_hash|
+      TeaShop.new(merchant_hash)
+    end
   end
 end
--- app/services/tea_shop_service.rb
+++ app/services/tea_shop_service.rb
@@ -4,6 +4,10 @@ class TeaShopService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def get_all_merchants
+    fetch_api("merchants")
+  end
+
   private
   def connection
     Faraday.new(url: "http://localhost:3000/api/v1/")
