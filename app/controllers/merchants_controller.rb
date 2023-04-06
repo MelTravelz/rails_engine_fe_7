5ea613a7ad5adf54ee9c04c87ae2b0764bf33448
Melony Franchini <meltravelz@gmail.com>
2023-04-05 20:35:46 -0600

    Refactor merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,12 +1,15 @@
 class MerchantsController < ApplicationController
 
   def index
-    tea_shop_facade = TeaShopFacade.new(params)
     @all_merchants = tea_shop_facade.get_all_merchants
   end
 
   def show
-    tea_shop_facade = TeaShopFacade.new(params)
     @merch_and_items = tea_shop_facade.get_merch_and_items
+  end
+
+  private
+  def tea_shop_facade
+    @tea_shop_facade ||= TeaShopFacade.new(params)
   end
 end
